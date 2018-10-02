b3dead8a55aea12d3d8faac6348b6178003cf795
Matt Lindsay <matt_epilp@hotmail.com>
2018-10-02 12:04:54 +0100

    Ready to test.

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
 const slack = require('./src/services/slack.js');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (res) => {
+app.get('/', (req, res) => {
   res.status(200).send('Welcome to my endpoint. It will return your http request for examination via Slack.');
 });
 
